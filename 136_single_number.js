4539a0e27aded79712ff55782bb5f6747e19d236
hoang bac <hoangvietbac090699@gmail.com>
2023-05-28 23:11:35 +0700

    compelte_136_137

--- 136_single_number.js
+++ 136_single_number.js
@@ -1,24 +1,12 @@
 //Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
 {
-    var nums1361 = [2, 1, 2];
-    var nums1362 = [9, 3, 2, 3, 2];
-    var nums1363 = [1];
-
-    function singleNumber(nums136) {
-        nums136.sort();
+    function singleNumber(nums) {
+        nums.sort();
         let result;
-        for (let i = 0; i <= nums136.length - 1; i += 2) {
-            if (nums136[i] !== nums136[i + 1]) {
-                return (result = nums136[i]);
+        for (let i = 0; i <= nums.length - 1; i += 2) {
+            if (nums[i] !== nums[i + 1]) {
+                return (result = nums[i]);
             }
         }
     }
-
-    let result1361 = singleNumber(nums1361);
-    let result1362 = singleNumber(nums1362);
-    let result1363 = singleNumber(nums1363);
-
-    console.log(`result_136_1: ${result1361}`);
-    console.log(`result_136_2: ${result1362}`);
-    console.log(`result_136_3: ${result1363}`);
 }
--- 137_single_number.js
+++ 137_single_number.js
@@ -1,21 +1,11 @@
 {
-    var nums1371 = [2, 2, 1, 2];
-    var nums1372 = [0, 1, 0, 1, 0, 1, 99];
-    var nums1373 = [0];
-    function singleNumber(nums137) {
-        nums137.sort();
+    function singleNumber(nums) {
+        nums.sort();
         let result;
-        for (let i = 0; i <= nums137.length - 1; i += 3) {
-            if (nums137[i] * 2 !== nums137[i + 1] + nums137[i + 2]) {
-                return (result = nums137[i]);
+        for (let i = 0; i <= nums.length - 1; i += 3) {
+            if (nums[i] * 2 !== nums[i + 1] + nums[i + 2]) {
+                return (result = nums[i]);
             }
         }
     }
-    let result1371 = singleNumber(nums1371);
-    let result1372 = singleNumber(nums1372);
-    let result1373 = singleNumber(nums1373);
-
-    console.log(`result_137_1: ${result1371}`);
-    console.log(`result_137_2: ${result1372}`);
-    console.log(`result_137_3: ${result1373}`);
 }
